384aec8586087acd38144dd6bd39ce39b38177a6
AchyutJadhav <achyut2106@gmail.com>
2024-04-17 08:12:39 +0530

    added more project on project page

--- frontend/src/Pages/ContactUs.js
+++ frontend/src/Pages/ContactUs.js
@@ -59,7 +59,7 @@ const ContactUs = () => {
           title="Google Map"
           src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d8163.671773021426!2d72.9952666971376!3d19.073042745524273!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x3be7c1361d0ca039%3A0x3ec38c6a67e95f69!2sVardhaman%20Market!5e0!3m2!1sen!2sin!4v1713013003498!5m2!1sen!2sin"
           width="100%"
-          height="450"
+          height="450px"
           style={{ border: 0 }}
           allowfullscreen=""
           loading="lazy"
